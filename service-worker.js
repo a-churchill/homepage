ac32424d27db776e469884c47dfe4b3b2ce09429
Andrew Churchill <adchurch@mit.edu>
2020-04-02 04:03:46 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4891713ca45cfec3b57790a61486b55.js"
+  "/precache-manifest.2b5df0226837a9a596c5020dfe19efb0.js"
 );
 
 self.addEventListener('message', (event) => {
