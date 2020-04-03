4ff91b4b69931e5055c84087f119cb9998914fa7
Andrew Churchill <adchurch@mit.edu>
2020-04-03 02:00:33 -0400

    Updates

---
+++ precache-manifest.439085d8d098951f14c792bccac0aa25.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3db4050f4ec8422052cda15fdf3dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c3bcfc4f2947849e01",
+    "url": "/static/css/main.06401b81.chunk.css"
+  },
+  {
+    "revision": "14d1160186af3e13f8e9",
+    "url": "/static/js/2.33429cfa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.33429cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c3bcfc4f2947849e01",
+    "url": "/static/js/main.e17f3dca.chunk.js"
+  },
+  {
+    "revision": "f2524c38ee08a8c13568",
+    "url": "/static/js/runtime-main.0ee01374.js"
+  },
+  {
+    "revision": "1d36554ce979f7e653ec4779f75b5c2b",
+    "url": "/static/media/andrew-churchill.1d36554c.jpg"
+  },
+  {
+    "revision": "5bb9dd421f244fa2d25a535b6a9796ae",
+    "url": "/static/media/andrew-churchill.5bb9dd42.jpg"
+  },
+  {
+    "revision": "a455a13b2563d2304d825c471e554d9d",
+    "url": "/static/media/concourse_c2_tab_bold-webfont.a455a13b.woff"
+  },
+  {
+    "revision": "d7257c74232e609db124c1e1c8918553",
+    "url": "/static/media/concourse_c2_tab_bold-webfont.d7257c74.woff2"
+  },
+  {
+    "revision": "2468d777e146aaf5d2221c5866888e82",
+    "url": "/static/media/concourse_c2_tab_regular-webfont.2468d777.woff"
+  },
+  {
+    "revision": "466a240e761942f86b8b7371145ca5e8",
+    "url": "/static/media/concourse_c2_tab_regular-webfont.466a240e.woff2"
+  },
+  {
+    "revision": "7e7c7cc0bc50656310733e1117567751",
+    "url": "/static/media/concourse_c3_tab_bold-webfont.7e7c7cc0.woff2"
+  },
+  {
+    "revision": "b866002394daf84094cf20a905f99814",
+    "url": "/static/media/concourse_c3_tab_bold-webfont.b8660023.woff"
+  },
+  {
+    "revision": "44e17dbe1d0deb32ad6cfc36de4da9e9",
+    "url": "/static/media/concourse_c3_tab_regular-webfont.44e17dbe.woff"
+  },
+  {
+    "revision": "87b61cc8c5235f5f384250fbae395b03",
+    "url": "/static/media/concourse_c3_tab_regular-webfont.87b61cc8.woff2"
+  },
+  {
+    "revision": "17e656d11c5568693c1bd02d05a0e80b",
+    "url": "/static/media/concourse_c4_tab_bold-webfont.17e656d1.woff2"
+  },
+  {
+    "revision": "32ff82ebada2bfe7fcdf7c5e9c52ed7c",
+    "url": "/static/media/concourse_c4_tab_bold-webfont.32ff82eb.woff"
+  },
+  {
+    "revision": "169bcae588308fe14bb3ffa4a0b14c20",
+    "url": "/static/media/concourse_c4_tab_regular-webfont.169bcae5.woff2"
+  },
+  {
+    "revision": "4c37479d5b651edbd04a2181524c157e",
+    "url": "/static/media/concourse_c4_tab_regular-webfont.4c37479d.woff"
+  },
+  {
+    "revision": "ac8b73ccb53ccb9ea1f913fd3bbd732e",
+    "url": "/static/media/concourse_t2_tab_bold-webfont.ac8b73cc.woff"
+  },
+  {
+    "revision": "d1769adc07c4af2cc20b2bf950018881",
+    "url": "/static/media/concourse_t2_tab_bold-webfont.d1769adc.woff2"
+  },
+  {
+    "revision": "7948b09aa7828183eef3fc4dcd5760df",
+    "url": "/static/media/concourse_t2_tab_bold_italic-webfont.7948b09a.woff2"
+  },
+  {
+    "revision": "d42c0cfabb11e256e8af7a109057c3c1",
+    "url": "/static/media/concourse_t2_tab_bold_italic-webfont.d42c0cfa.woff"
+  },
+  {
+    "revision": "427dbe4a5e7223c7d0f69f56cc993e4f",
+    "url": "/static/media/concourse_t2_tab_italic-webfont.427dbe4a.woff"
+  },
+  {
+    "revision": "f8ce50c93f253ed0137346014be8f6dc",
+    "url": "/static/media/concourse_t2_tab_italic-webfont.f8ce50c9.woff2"
+  },
+  {
+    "revision": "256eacafdc56d4506ce3a0b7a03df3a5",
+    "url": "/static/media/concourse_t2_tab_regular-webfont.256eacaf.woff"
+  },
+  {
+    "revision": "ae54b2a30157ba6e1f9cecf4bab0e427",
+    "url": "/static/media/concourse_t2_tab_regular-webfont.ae54b2a3.woff2"
+  },
+  {
+    "revision": "1722ee09307a135443c19bac7e673acd",
+    "url": "/static/media/concourse_t3_tab_bold-webfont.1722ee09.woff"
+  },
+  {
+    "revision": "cfa1f68799efe3ae7b4cec61b1c36dc2",
+    "url": "/static/media/concourse_t3_tab_bold-webfont.cfa1f687.woff2"
+  },
+  {
+    "revision": "10510536f1065d7be99ba0b6759a3970",
+    "url": "/static/media/concourse_t3_tab_bold_italic-webfont.10510536.woff"
+  },
+  {
+    "revision": "25d6b507fd7f63b8efcc55d9b933faa3",
+    "url": "/static/media/concourse_t3_tab_bold_italic-webfont.25d6b507.woff2"
+  },
+  {
+    "revision": "e6cc9c50903ace5cc09ca8cce7bc378d",
+    "url": "/static/media/concourse_t3_tab_italic-webfont.e6cc9c50.woff"
+  },
+  {
+    "revision": "f54ea4f65ff3e6ee350fd3c90d0fec00",
+    "url": "/static/media/concourse_t3_tab_italic-webfont.f54ea4f6.woff2"
+  },
+  {
+    "revision": "2ac5e1f6505e3bc00255cb9b19fae779",
+    "url": "/static/media/concourse_t3_tab_regular-webfont.2ac5e1f6.woff"
+  },
+  {
+    "revision": "4342925b2a7888667bd285253e9e884c",
+    "url": "/static/media/concourse_t3_tab_regular-webfont.4342925b.woff2"
+  },
+  {
+    "revision": "170cfe03dd75a2af57a368c5a820ef0b",
+    "url": "/static/media/embarcadero.170cfe03.jpg"
+  },
+  {
+    "revision": "8c9fba6d8f640fee0dc9fb67c86ec676",
+    "url": "/static/media/equity_caps_a_bold-webfont.8c9fba6d.woff"
+  },
+  {
+    "revision": "de188557a8584c516a7c41cee0548e50",
+    "url": "/static/media/equity_caps_a_bold-webfont.de188557.woff2"
+  },
+  {
+    "revision": "7db380d138f559c72d4f6efa2c9fa959",
+    "url": "/static/media/equity_caps_a_regular-webfont.7db380d1.woff2"
+  },
+  {
+    "revision": "d714ac9a6d0e6c10286ba726c2143d37",
+    "url": "/static/media/equity_caps_a_regular-webfont.d714ac9a.woff"
+  },
+  {
+    "revision": "4c94173e9533628287b4b1870be3a9b4",
+    "url": "/static/media/equity_text_a_bold-webfont.4c94173e.woff"
+  },
+  {
+    "revision": "cd38c640e61081e60d21e7df100f8407",
+    "url": "/static/media/equity_text_a_bold-webfont.cd38c640.woff2"
+  },
+  {
+    "revision": "1849b1fd8cc35d0742f29154dd7bdf29",
+    "url": "/static/media/equity_text_a_bold_italic-webfont.1849b1fd.woff"
+  },
+  {
+    "revision": "25ea2b39e500eb20d3596575e1032827",
+    "url": "/static/media/equity_text_a_bold_italic-webfont.25ea2b39.woff2"
+  },
+  {
+    "revision": "2f93d0ed004e6270a2d95a251e7c9a63",
+    "url": "/static/media/equity_text_a_italic-webfont.2f93d0ed.woff2"
+  },
+  {
+    "revision": "b98e414081a40ee6ed5824b99a2da339",
+    "url": "/static/media/equity_text_a_italic-webfont.b98e4140.woff"
+  },
+  {
+    "revision": "b303f4198054290ed2acffe0953bd4d6",
+    "url": "/static/media/equity_text_a_regular-webfont.b303f419.woff2"
+  },
+  {
+    "revision": "d95308920e9d55cb39665f365d02f9aa",
+    "url": "/static/media/equity_text_a_regular-webfont.d9530892.woff"
+  },
+  {
+    "revision": "e544df6c96a3807d8d98911ebf6e827c",
+    "url": "/static/media/farm.e544df6c.jpg"
+  },
+  {
+    "revision": "4416fee02ec65c32ec2d8a95d160fbfa",
+    "url": "/static/media/golden-gate.4416fee0.jpg"
+  },
+  {
+    "revision": "00260d4ccc3d0cc7c2b1ec0511e6d331",
+    "url": "/static/media/icomoon.00260d4c.ttf"
+  },
+  {
+    "revision": "151c4cdf48007db6459d6c381b6aef79",
+    "url": "/static/media/icomoon.151c4cdf.woff"
+  },
+  {
+    "revision": "5616ed32ee28a7855c82d72c994e1fa7",
+    "url": "/static/media/icomoon.5616ed32.svg"
+  },
+  {
+    "revision": "83c22879d6a99cb36f77e30301f588a6",
+    "url": "/static/media/icomoon.83c22879.eot"
+  },
+  {
+    "revision": "19c7a9f42a1d65d4ed993df4c4d1cb04",
+    "url": "/static/media/la-jolla.19c7a9f4.jpg"
+  },
+  {
+    "revision": "287c1c54f2177899616a5440df2a8588",
+    "url": "/static/media/la-jolla.287c1c54.jpg"
+  },
+  {
+    "revision": "ece4a61759f7e7d752278d216c455965",
+    "url": "/static/media/lake-tahoe.ece4a617.jpg"
+  },
+  {
+    "revision": "188da2d3ef0dee14ffa04ee0358b1c43",
+    "url": "/static/media/light-string.188da2d3.jpg"
+  },
+  {
+    "revision": "4270bd3712ba8d46cdfa54c45b9b0e2d",
+    "url": "/static/media/louvre-inside.4270bd37.jpg"
+  },
+  {
+    "revision": "6b0e0871e258168508bb3899f537d190",
+    "url": "/static/media/louvre-inside.6b0e0871.jpg"
+  },
+  {
+    "revision": "5ddd60d3e5d822dd2cfe89ae0ed9d630",
+    "url": "/static/media/louvre-outside.5ddd60d3.jpg"
+  },
+  {
+    "revision": "f48355a3c9da59cd45965cf768ea9d1e",
+    "url": "/static/media/palm-tree-sunset.f48355a3.jpg"
+  },
+  {
+    "revision": "522cf3b7bd7e34753522746221cda9e0",
+    "url": "/static/media/palm-tree.522cf3b7.jpg"
+  },
+  {
+    "revision": "771c43af705aec303537504b742909be",
+    "url": "/static/media/sausalito.771c43af.jpg"
+  },
+  {
+    "revision": "da40912471db892bd2d268cf58228560",
+    "url": "/static/media/sausalito.da409124.jpg"
+  },
+  {
+    "revision": "7dde7ff617cf0db9a9fbf3123ddd2e81",
+    "url": "/static/media/ships.7dde7ff6.jpg"
+  },
+  {
+    "revision": "93d3a147c51a1ab790acd5ac0d965550",
+    "url": "/static/media/wadi-rum.93d3a147.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b5df0226837a9a596c5020dfe19efb0.js"
+  "/precache-manifest.439085d8d098951f14c792bccac0aa25.js"
 );
 
 self.addEventListener('message', (event) => {
